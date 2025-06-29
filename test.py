e4bedd8f2059763af5ff1bf10bce4a79c410cbbb
linuxistliebe <linuxistliebe@gmail.com>
2025-06-29 22:36:21 +0530

    woah, so many changes!

--- test.py
+++ test.py
@@ -30,25 +30,123 @@ WELCOME TO MSD-RAILWAYS HOME
 
     print()
     typfast('''
-[1] Tickets 
-[2] Login
-[3] Register 
-[4] Help
-[5] Credits
-[6] Quit                  
+[1] Login
+[2] Register 
+[3] Help
+[4] Credits
+[5] Quit                  
 ''')
+
+
+
+
+    def logindash():
+        typ("(*)Logged in successfully")
+        print()
+        while True:
+                typfast('''
+
+--------------------      
+WELCOME TO DASHBOARD
+--------------------
+''')
+
+                typfast('''
+[1] Reservation
+[2] View current_ticket_status
+[3] Cancellation
+[4] Return to home                  
+''')
+                def reservation():
+                    print()
+                    
+                    print('''
+-----------------
+AVAILABLE ROUTES|
+-----------------
+                        ''')
+                    typfast('''
+[1] Gotham to Empire Bay      7:00 AM
+[2] Gotham to Metropolis      7:00 AM 
+[3] Gotham to Vice City       7:00 AM  
+[4] Empire Bay to Gotham      7:00 AM  
+[5] Metropolis to Gotham      7:00 AM  
+[6] Vice City to Gotham       7:00 AM  
+[7] Empire Bay to Metropolis  7:00 AM  
+[8] Metropolis to Vice City   7:00 AM
+[9] <--Return  
+''')
+                    time.sleep(0.4)
+                    tkt_choice=int(input("Select a TICKET to book -> "))
+                    if tkt_choice==9:
+                        pass
+                    elif tkt_choice==8:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==7:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==6:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==5:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==4:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==3:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==2:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    elif tkt_choice==1:
+                        print("Ticket No.",tkt_choice,"booked successfully!")
+                        time.sleep(0.8)
+
+                    else:
+                        typ("(*)Invalid Choice Entered!")
+                        time.sleep(1.2)
+
+                choice_dash=int(input("Enter your choice -> "))
+                if choice_dash==4:
+                    break
+                elif choice_dash==3:
+                    typ("|CANCELLATION|")
+                    print()
+                elif choice_dash==2:
+                    typ("|current_ticket_status|")
+                    print()
+
+                elif choice_dash==1:
+                    typ("|Reservation|")
+                    reservation()
+
+                else:
+                    typ("(*)Invalid Choice Entered!")
+                    time.sleep(1.8)
+
+
     choice=int(input("Enter your choice -> "))
-    if choice==6:
+    if choice==5:
         typ("Quitting MSD-RAILWAYS Home...")
         exit()
-    elif choice==5:
+    elif choice==4:
         typ(" GITHUB- https://github.com/linuxistliebe ")
         time.sleep(5)
 
-    elif choice==4:
+    elif choice==3:
         typ("This is MSD-Railways Home. Here, you can open the Tickets Dashboard, login to your account (if exists), register an account, open the credits panel.")
         time.sleep(3)
-    elif choice==2:
+    elif choice==1:
         print()
         username_query=input("Enter your USERNAME -> ")
         password_query=input("Enter your PASSWORD -> ")
@@ -60,12 +158,12 @@ WELCOME TO MSD-RAILWAYS HOME
 
         else:
             time.sleep(0.8)
-            for i in result:
-                print(i)
+            logindash()
+            
 
         time.sleep(2)
         
-    elif choice==3:
+    elif choice==2:
         print()
         username_create=input("Create a Username -> ")
         password_create=input("Create a Password -> ")
@@ -83,6 +181,6 @@ WELCOME TO MSD-RAILWAYS HOME
             print("USERNAME -",username_create,"is not available!")
             time.sleep(1.3)
     else:
-        typ("Invalid Choice Entered!")
-        time.sleep(2)
+        typ("(*)Invalid Choice Entered!")
+        time.sleep(1.8)
         
