aa040937bcc0cc97414a44dad35aa32e90aca3a4
linuxistliebe <linuxistliebe@gmail.com>
2025-07-01 00:21:20 +0530

    forgot to commit and push to repo on 30 june. it is 12:21 am right now

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#importing modules and establishing connection
 import time
 import mysql.connector as msd
 con=msd.connect(host='localhost', user='root', password='seven', database='msd_db')
@@ -8,6 +9,7 @@ else:
 
 curs=con.cursor()
 
+#a function to get that "typewriter effect"
 def typ(a):
     for i in a:
         print(i,end="")
@@ -18,7 +20,7 @@ def typfast(a):
         time.sleep(0.003)
 
 
-
+# a while loop for the main menu of "MSD-RAILWAYS"
 while True:
     typfast('''
 
@@ -39,10 +41,10 @@ WELCOME TO MSD-RAILWAYS HOME
 
 
 
-
-    def logindash():
-        typ("(*)Logged in successfully")
-        print()
+    # logindash is a function for the dashboard displayed after logging in
+    def logindash(loggedin_id):
+        typ("(*)Logged in successfully as ")
+        print(loggedin_id)
         while True:
                 typfast('''
 
@@ -57,6 +59,26 @@ WELCOME TO DASHBOARD
 [3] Cancellation
 [4] Return to home                  
 ''')
+                # this function is for viewing booked tickets by the user
+                def current_ticket_status():
+                    print()
+                    curs.execute("select * from {}".format(loggedin_id))
+                    result_current_ticket_status=curs.fetchall()
+                    if result_current_ticket_status==None:
+                        print("(*) No Tickets have been booked with this account!")
+                        time.sleep(1.2)
+
+                    else:
+                        for i in result_current_ticket_status:
+                            print(i)
+
+
+
+                # this function is for cancellation of tickets booked by the user
+                def cancellation():
+                    print()
+
+                # this is a function for booking tickets from available options
                 def reservation():
                     print()
                     
@@ -74,7 +96,7 @@ AVAILABLE ROUTES|
 [6] Vice City to Gotham       7:00 AM  
 [7] Empire Bay to Metropolis  7:00 AM  
 [8] Metropolis to Vice City   7:00 AM
-[9] <--Return  
+[9] <--Return    
 ''')
                     time.sleep(0.4)
                     tkt_choice=int(input("Select a TICKET to book -> "))
@@ -83,49 +105,73 @@ AVAILABLE ROUTES|
                     elif tkt_choice==8:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (8,100,'Metropolis','Vice City','7 AM','Flashpoint')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==7:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (7,100,'Empire Bay','Metropolis','7 AM','Flashpoint')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==6:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (6,100,'Vice City','Gotham','7 AM','Flashpoint')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==5:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (5,100,'Metropolis','Gotham','7 AM','Flashpoint')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==4:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (4,100,'Empire Bay','Gotham','7 AM','Flashpoint')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==3:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (3,50,'Gotham','Vice City','7 AM','LinuxEXP')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==2:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (2,50,'Gotham','Metropolis','7 AM','LinuxEXP')".format(loggedin_id))
+                        con.commit()
 
                     elif tkt_choice==1:
                         print("Ticket No.",tkt_choice,"booked successfully!")
                         time.sleep(0.8)
+                        curs.execute("insert into {} values (1,50,'Gotham','Empire Bay','7 AM','LinuxEXP')".format(loggedin_id))
+                        con.commit()
 
                     else:
                         typ("(*)Invalid Choice Entered!")
                         time.sleep(1.2)
 
                 choice_dash=int(input("Enter your choice -> "))
+                
+                #this one signs you out
                 if choice_dash==4:
                     break
+
+                # function for cancelling the previously books tickets by a user
                 elif choice_dash==3:
                     typ("|CANCELLATION|")
                     print()
+
+                #displays currently booked tickets by a user
                 elif choice_dash==2:
                     typ("|current_ticket_status|")
                     print()
+                    current_ticket_status()
 
+                # takes you to the reservation function where you book tickets from available option
                 elif choice_dash==1:
                     typ("|Reservation|")
                     reservation()
@@ -134,7 +180,7 @@ AVAILABLE ROUTES|
                     typ("(*)Invalid Choice Entered!")
                     time.sleep(1.8)
 
-
+    # if-elif-else statements for the MSD-RAILWAYS menu options
     choice=int(input("Enter your choice -> "))
     if choice==5:
         typ("Quitting MSD-RAILWAYS Home...")
@@ -146,6 +192,8 @@ AVAILABLE ROUTES|
     elif choice==3:
         typ("This is MSD-Railways Home. Here, you can open the Tickets Dashboard, login to your account (if exists), register an account, open the credits panel.")
         time.sleep(3)
+
+    # this one is for loggin in, checks username and password and drops you to the dashboard mentioned previously
     elif choice==1:
         print()
         username_query=input("Enter your USERNAME -> ")
@@ -158,11 +206,12 @@ AVAILABLE ROUTES|
 
         else:
             time.sleep(0.8)
-            logindash()
+            logindash(username_query)
             
 
         time.sleep(2)
-        
+
+    #this one is for creating an account    
     elif choice==2:
         print()
         username_create=input("Create a Username -> ")
@@ -176,10 +225,12 @@ AVAILABLE ROUTES|
             curs.execute("insert into account values('{}','{}','{}','{}','{}')".format(name_create,username_create,age_create,city_create,password_create))     
             con.commit()
             time.sleep(1.7)
+            curs.execute("create table '{}'(num int, price int, from_city varchar(30), to_city varchar(30), time varchar(10), train_name varchar(10))".format(username_create))
             print("Account Created","(*)USERNAME -",username_create)
         else:
             print("USERNAME -",username_create,"is not available!")
             time.sleep(1.3)
+    # invalid input given by user in MSD-RAILWAYS menu
     else:
         typ("(*)Invalid Choice Entered!")
         time.sleep(1.8)
